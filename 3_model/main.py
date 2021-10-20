73950b5cb162e29c682afff85ef33c2bca572ecd
ry4git <400mhs2@gmail.com>
2021-10-19 21:06:58 -0700

    fixed the mass balance, added AET module

---
+++ 3_model/main.py
@@ -0,0 +1,24 @@
+import os
+import sys
+sys.path.append("G:\\Shared drives\\Ryoko and Hilary\\SMSigxModel\\analysis\\3_model\\libs")
+import cfe
+
+# specify current directory create output directory if it does not exist
+os.chdir('G:\\Shared drives\\Ryoko and Hilary\\SMSigxModel\\analysis\\3_model')
+os.getcwd()
+out_file_path = '.\\out\\'
+if not os.path.exists(out_file_path):
+    os.mkdir(out_file_path)
+
+data_file_path = '.\\data\\'
+
+def main():
+    cfe1 = cfe.CFE(os.path.join(data_file_path, 'cat_58_config_cfe.json'))
+    cfe1.run_unit_test()
+    cfe1.initialize()
+    cfe1.update()
+    cfe1.update_until(4)
+    cfe1.finalize()
+
+if __name__ == '__main__':
+    main()
