39d0b6cb788d60a1328cb156ec824614333b2aa1
ry4git <400mhs2@gmail.com>
2022-03-22 12:41:11 -0700

    added plotting. debugged triangler func

--- 3_model/libs/glue_cfe.py
+++ 3_model/libs/glue_cfe.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 # Global variables
 var_names = ["Flow", "Soil Moisture Content"] # variables of interst
-eval_names = ['KGE_Q', 'KGE_SM', 'd2w_start', 'd2w_end', 'w2d_start', 'w2d_end'] # evaluators except seasontrans
+eval_names = ['KGE_Q', 'KGE_SM', 'd2w_start', 'd2w_end', 'w2d_start', 'w2d_end'] # evaluators including seasontrans
 KGE_Q_thresh = 0.3 # threshold value
 KGE_SM_thresh = 0.3 # threshold value
 seasontrans_thresh = 100 # seasonal transition threshold
@@ -62,7 +62,7 @@ def triangle_weight(x, a, b, c):
     #for i in range(len(x)):
     y = np.where((x<=a) | (x>=c), 0, y)
     y = np.where((a<=x) & (x<=b), (x-a)/(b-a), y)
-    y = np.where((b<=x) & (x<=c), (b-x)/(c-b), y)
+    y = np.where((b<=x) & (x<=c), (c-x)/(c-b), y)
     return y
 
 # GLUE object
@@ -104,7 +104,7 @@ class MyGLUE(object):
     def simulation(self):
     # run the simulation
         print('--- Running CFE model ---')
-        m = 0
+        flag_behavioral = 0
         for n in range(self.nrun):
 
             print('{}-th run'.format(n))
@@ -133,7 +133,7 @@ class MyGLUE(object):
             # ===============================================================
             # Actual model run
             # ===============================================================
-            # self.myCFE.initialize()
+            self.myCFE.initialize()
             sim0 = self.myCFE.run_unit_test(plot=False)
             obs0 = self.myCFE.load_unit_test_data()
 
@@ -161,12 +161,13 @@ class MyGLUE(object):
                 # Seasonsig for SM data
                 if var_name == "Soil Moisture Content":
 
-                    # Observed
-                    sig_obs = SMSig(ts_time=df["Time"].to_numpy(), ts_value=obs_synced.to_numpy(), plot_results=False)
-                    # sig_obs.detrend() # TODO:debug
-                    sig_obs.movmean()
-                    t_valley = sig_obs.calc_sinecurve()
-                    season_trans_obs = sig_obs.calc_seasontrans(t_valley=t_valley)
+                    # Observed. Run only once
+                    if n == 0:
+                        sig_obs = SMSig(ts_time=df["Time"].to_numpy(), ts_value=obs_synced.to_numpy(), plot_results=False)
+                        # sig_obs.detrend() # TODO:debug
+                        sig_obs.movmean()
+                        t_valley = sig_obs.calc_sinecurve()
+                        season_trans_obs = sig_obs.calc_seasontrans(t_valley=t_valley)
 
                     # simulated
                     sig_sim = SMSig(ts_time=df["Time"].to_numpy(), ts_value=sim_synced.to_numpy(), plot_results=False)
@@ -204,13 +205,13 @@ class MyGLUE(object):
                 # Store the behavioral runs
                 self.post_rid.append(n) #runid
                 self.post_paras.append(self.sampled) #parameters
-                if m == 0:
+                if flag_behavioral == 0:
                     self.df_Q_behavioral = sim_Q_synced #timeseires
                     self.df_SM_behavioral = sim_SM_synced
                 else:
                     self.df_Q_behavioral = pd.concat([self.df_Q_behavioral, sim_Q_synced], axis=1)
                     self.df_SM_behavioral = pd.concat([self.df_SM_behavioral, sim_SM_synced], axis=1)
-                m = 1
+                flag_behavioral = 1
                 self.eval.append([KGE_Q, KGE_SM, diff_avg[0], diff_avg[1], diff_avg[2], diff_avg[3]])
             else:
                 # Discard non-behavioral runs
@@ -276,7 +277,6 @@ class MyGLUE(object):
         # Soil moisture
         self.df_SM_obs = pd.DataFrame(obs_SM_synced)
         self.df_SM_obs.set_axis(df["Time"], axis=0, inplace=True)
-        # TODO: Check observed values
 
     def post_process(self):
         # post-process the results
@@ -289,10 +289,13 @@ class MyGLUE(object):
             # Get empirical weight
             KGE_Q_weight = (self.df_post_eval["KGE_Q"] - KGE_Q_thresh)/sum( (self.df_post_eval["KGE_Q"] - KGE_Q_thresh))
             KGE_SM_weight = (self.df_post_eval["KGE_SM"] - KGE_SM_thresh) / sum((self.df_post_eval["KGE_SM"] - KGE_SM_thresh))
-            # TODO: Get triangular weight for seasontrans
+            Seasonsig_weight_d2ws = triangle_weight(self.df_post_eval['d2w_start'], a= -1*seasontrans_thresh, b=0, c=seasontrans_thresh)
+            Seasonsig_weight_d2we = triangle_weight(self.df_post_eval['d2w_end'], a=-1 * seasontrans_thresh, b=0, c=seasontrans_thresh)
+            Seasonsig_weight_w2ds = triangle_weight(self.df_post_eval['w2d_start'], a=-1 * seasontrans_thresh, b=0, c=seasontrans_thresh)
+            Seasonsig_weight_w2de = triangle_weight(self.df_post_eval['w2d_end'], a=-1 * seasontrans_thresh, b=0, c=seasontrans_thresh)
 
             # Get composite weight
-            weight = KGE_Q_weight.values + KGE_SM_weight.values
+            weight = (KGE_Q_weight.values + KGE_SM_weight.values + Seasonsig_weight_d2ws + Seasonsig_weight_d2we + Seasonsig_weight_w2ds + Seasonsig_weight_w2de)/6
 
             # Calculate weighted quantile
             for var_name in var_names:
@@ -329,67 +332,118 @@ class MyGLUE(object):
         if hasattr(self, 'df_SM_simrange'):
             self.df_SM_simrange.to_csv(os.path.join(self.out_path, 'quantiles_SM.csv'), sep=',', header=True, index=False, encoding='utf-8', na_rep='nan')
 
-    def plot(self):
+    def plot(self, plot_type=None):
         # Plot the results
         print('--- Saving the plots ---')
 
         # Prior vs. posterior parameter distributions
-        nparas = len(self.df_pri_paras.columns)
-        f = plt.figure(figsize=(4*4, 4*4))
+        if plot_type == "param_hist":
+            nparas = len(self.df_pri_paras.columns)
+            f = plt.figure(figsize=(4*4, 4*4))
 
-        for i in range(nparas):
-            target_para = self.df_pri_paras.columns[i]
-            ax1 = f.add_subplot(4, 4, i+1)
+            for i in range(nparas):
+                target_para = self.df_pri_paras.columns[i]
+                ax1 = f.add_subplot(4, 4, i+1)
 
-            self.df_pri_paras[target_para].plot.hist(bins=10, alpha=0.4, ax=ax1, color="#3182bd", label="Prior")
+                self.df_pri_paras[target_para].plot.hist(bins=10, alpha=0.4, ax=ax1, color="#3182bd", label="Prior")
+
+                if hasattr(self, 'df_post_paras'):
+                    self.df_post_paras[target_para].plot.hist(bins=10, alpha=0.8, ax=ax1, color="#3182bd", label="Posterior")
+
+                ax1.set_xlabel(target_para)
+                ax1.legend()
+
+                if i !=0:
+                    ax1.yaxis.set_visible(False)
+
+            # f.plot()
+
+            f.savefig(os.path.join(self.out_path, 'param_dist.png'), dpi=600)
+
+
+        # Dotty plot
+        # Prior vs. posterior parameter distributions
+        if plot_type == "dotty":
+            if hasattr(self, 'df_post_paras'):
+
+                nparas = len(self.df_pri_paras.columns)
+
+                for j in range(len(self.df_post_eval.columns)):
+                    f = plt.figure(figsize=(4 * 4, 4 * 4))
+                    target_eval = self.df_post_eval.columns[j]
+                    for i in range(nparas):
+                        target_para = self.df_pri_paras.columns[i]
+                        ax1 = f.add_subplot(4, 4, i+1)
+                        plt.scatter(self.df_post_paras[target_para], self.df_post_eval[target_eval])
+
+                    ax1.set_xlabel(target_para)
+                    ax1.set_ylabel(target_eval)
+
+                    if i !=0:
+                        ax1.yaxis.set_visible(False)
+
+                    # f.plot()
+                    f.savefig(os.path.join(self.out_path, "param_dotty_%s.png" % (target_eval)), dpi=600)
+
+        if plot_type == "dotty_interaction":
+            param_combi = [['fc_atm_press_fraction', 'satdk'],
+                           ['fc_atm_press_fraction', 'bb'],
+                           ['fc_atm_press_fraction', 'smcmax']]
 
             if hasattr(self, 'df_post_paras'):
-                self.df_post_paras[target_para].plot.hist(bins=10, alpha=0.8, ax=ax1, color="#3182bd", label="Posterior")
+                    f = plt.figure(figsize=(4 * 4, 4 * 4))
+                    for i in range(len(param_combi)):
+                        para0 = param_combi[i][0]
+                        para1 = param_combi[i][1]
+                        ax1 = f.add_subplot(4, 4, i+1)
+                        x = self.df_post_paras[para0]
+                        y = self.df_post_paras[para1]
+                        plt.scatter(x, y)
 
-            ax1.set_xlabel(target_para)
-            ax1.legend()
+                        ax1.set_xlabel(para0)
+                        ax1.set_ylabel(para1)
 
-            if i !=0:
-                ax1.yaxis.set_visible(False)
+                        if i !=0:
+                            ax1.yaxis.set_visible(False)
 
-        # f.plot()
-
-        f.savefig(os.path.join(self.out_path, 'param_dist.png'), dpi=600)
+                        # f.plot()
+                        f.savefig(os.path.join(self.out_path, "param_dotty__%s__%s.png" % (para0, para1)), dpi=600)
 
         # Time series of data
-        if hasattr(self, 'df_Q_simrange'):
-            for var_name in var_names:
-                if var_name == "Flow":
-                    df_simrange = self.df_Q_simrange
-                    df_obs = self.df_Q_obs
-                    obs_label = 'Observed flow'
-                    y_label = 'Total flow [mm/hour]'
-                    title = 'Total flow for behavioral runs'
-                    fn = 'Q_range.png'
-                elif var_name == "Soil Moisture Content":
-                    df_simrange = self.df_SM_simrange
-                    df_obs = self.df_SM_obs
-                    obs_label = 'Observed soil moisture'
-                    y_label = 'Volmetric Soil Moisture Content [m^3/m^3]'
-                    title = 'Soil moisture for behavioral runs'
-                    fn = 'SM_range.png'
+        if plot_type == "timeseries":
+            if hasattr(self, 'df_Q_simrange'):
+                for var_name in var_names:
+                    if var_name == "Flow":
+                        df_simrange = self.df_Q_simrange
+                        df_obs = self.df_Q_obs
+                        obs_label = 'Observed flow'
+                        y_label = 'Total flow [mm/hour]'
+                        title = 'Total flow for behavioral runs'
+                        fn = 'Q_range.png'
+                    elif var_name == "Soil Moisture Content":
+                        df_simrange = self.df_SM_simrange
+                        df_obs = self.df_SM_obs
+                        obs_label = 'Observed soil moisture'
+                        y_label = 'Volmetric Soil Moisture Content [m^3/m^3]'
+                        title = 'Soil moisture for behavioral runs'
+                        fn = 'SM_range.png'
 
-                f2 = plt.figure(figsize=(8,6))
-                ax2 = f2.add_subplot()
+                    f2 = plt.figure(figsize=(8,6))
+                    ax2 = f2.add_subplot()
 
-                df_simrange['lowerlim'].plot(color='black',alpha=0.2, ax= ax2,  label='_Hidden')
-                df_simrange['upperlim'].plot(color='black', alpha=0.2, ax=ax2,  label='_Hidden')
-                df_obs[var_name + "_y"].plot(color='black', alpha=1, ax=ax2, label=obs_label)
-                plt.fill_between(df_simrange.index, df_simrange['upperlim'], df_simrange['lowerlim'],
-                                 facecolor='green', alpha=0.2, interpolate=True, label='Predicted range')
-                # if var_name == "Flow":
-                #     ax2.set_yscale('log')
-                ax2.set_xlabel('Time')
-                ax2.set_ylabel(y_label)
-                ax2.set_title(title)
-                ax2.legend()
-                f2.savefig(os.path.join(self.out_path, fn), dpi=600)
+                    df_simrange['lowerlim'].plot(color='black',alpha=0.2, ax= ax2,  label='_Hidden')
+                    df_simrange['upperlim'].plot(color='black', alpha=0.2, ax=ax2,  label='_Hidden')
+                    df_obs[var_name + "_y"].plot(color='black', alpha=1, ax=ax2, label=obs_label)
+                    plt.fill_between(df_simrange.index, df_simrange['upperlim'], df_simrange['lowerlim'],
+                                     facecolor='green', alpha=0.2, interpolate=True, label='Predicted range')
+                    # if var_name == "Flow":
+                    #     ax2.set_yscale('log')
+                    ax2.set_xlabel('Time')
+                    ax2.set_ylabel(y_label)
+                    ax2.set_title(title)
+                    ax2.legend()
+                    f2.savefig(os.path.join(self.out_path, fn), dpi=600)
 
-                # f2.plot()
+                    # f2.plot()
 
 
