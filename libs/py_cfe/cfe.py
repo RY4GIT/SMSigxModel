Fixed the infiltration scaling errors

libs/py_cfe/cfe.py
@@ -429,13 +429,24 @@ class CFE:
         sum_outflux = lateral_flux_frac + perc_flux_frac + et_from_soil_frac
         if sum_outflux.any() == 0:
             flux_scale = 0
+            if cfe_state.infiltration_depth_m > 0:
+                # To account for mass balance error by ODE
+                final_storage_m = y0[0] + cfe_state.infiltration_depth_m
+            else:
+                final_storage_m = y0[0]
         else:
-            flux_scale = np.zeros(infilt_to_soil_frac.shape)
-            flux_scale[sum_outflux != 0] = (
-                np.diff(-ys_concat, axis=0)[sum_outflux != 0]
-                + infilt_to_soil_frac[sum_outflux != 0]
-            ) / sum_outflux[sum_outflux != 0]
-            flux_scale[sum_outflux == 0] = 0
+            flux_scale = (
+                (ys_concat[0] - ys_concat[-1]) + np.sum(infilt_to_soil_frac)
+            ) / np.sum(sum_outflux)
+            final_storage_m = ys_concat[-1]
+
+            # flux_scale = np.zeros(infilt_to_soil_frac.shape)
+            # flux_scale[sum_outflux != 0] = (
+            #     np.diff(-ys_concat, axis=0)[sum_outflux != 0]
+            #     + infilt_to_soil_frac[sum_outflux != 0]
+            # ) / sum_outflux[sum_outflux != 0]
+            # flux_scale[sum_outflux == 0] = 0
+
         scaled_lateral_flux = lateral_flux_frac * flux_scale
         scaled_perc_flux = perc_flux_frac * flux_scale
         scaled_et_flux = et_from_soil_frac * flux_scale
@@ -444,7 +455,7 @@ class CFE:
         cfe_state.primary_flux_m = math.fsum(scaled_perc_flux)
         cfe_state.secondary_flux_m = math.fsum(scaled_lateral_flux)
         cfe_state.actual_et_from_soil_m_per_timestep = math.fsum(scaled_et_flux)
-        reservoir["storage_m"] = ys_concat[-1]
+        reservoir["storage_m"] = final_storage_m
 
         """
         # Comment out because this section raises Runtime error, as dS_soil_reservoir is extremely small