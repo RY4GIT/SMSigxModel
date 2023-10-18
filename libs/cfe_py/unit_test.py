ff976fe770b3b9b7088af6e7cf91aacc565771f8
RY4GIT <400mhs2@gmail.com>
2023-10-18 16:54:48 -0700

    fix: unit

--- libs/cfe_py/unit_test.py
+++ libs/cfe_py/unit_test.py
@@ -24,36 +24,33 @@ from bmi_cfe import BMI_CFE
 
 input_json = "./data/unit_test/config_cfe.json"
 params = {
-    "forcing_file": "G:/Shared drives/Ryoko and Hilary/SMSigxModel/analysis/data/LittleWashita/little_washita_2006_2012.csv",
-    "catchment_area_km2": 601,
+    "forcing_file": "G:/Shared drives/Ryoko and Hilary/SMSigxModel/analysis/data/Coweeta/forcing_daily_2014_2018.csv",
+    "catchment_area_km2": 0.1210,
     "soil_params": {
-        "bb": 4.74,
-        "satdk": 1e-02,
-        "slop": 1,
-        "satpsi": 0.141,
-        "smcmax": 0.4,
-        "wltsmc": 0.05,
+        "bb": 8.17,
+        "satdk": 2.45e-06,
+        "slop": 0.147,
+        "satpsi": 0.263,
+        "smcmax": 0.5,
+        "wltsmc": 0.35,
         "D": 2,
-        "exponent_secondary": 0.71689,
     },
-    "allow_percolation_below_threshold": 0,
-    "alpha_fc": 0.18,
-    "max_gw_storage": 1,
-    "Cgw": 1e-04,
-    "expon": 0.60668 * 2,
-    "gw_scheme": "Linear",
-    "K_lf": 1e-03,
-    "refkdt": 500,
-    "K_nash": 0.01,
-    "trigger_z_fact": 0.25,
-    "num_nash_storage": 250,
-    "giuh_ordinates": [0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.4, 0.2, 0.2, 0.1],
+    "max_gw_storage": 50,
+    "Cgw": 1,
+    "expon": 1.75,
+    "K_lf": 0.5,
+    "K_nash": 0.1,
+    "num_nash_storage": 2,
+    "giuh_ordinates": [0.5, 0.5],
+    "trigger_z_fact": 0.5,
+    "alpha_fc": 0.33,
+    "refkdt": 3,
     "stand_alone": 1,
     "unit_test": 1,
-    # "revap_factor": 0.05,
-    "compare_results_file": "G:/Shared drives/Ryoko and Hilary/SMSigxModel/analysis/data/LittleWashita/test_sm_basinavg.csv",
+    "time_step_size": 86400,
+    "gw_scheme": "Exponential",
+    "compare_results_file": "G:/Shared drives/Ryoko and Hilary/SMSigxModel/analysis/data/Coweeta/test_daily_2014_2018_sm_basinavg.csv",
 }
-
 with open(input_json, "w") as outfile:
     json.dump(params, outfile, indent=4)
 
@@ -69,9 +66,9 @@ cfe_instance.initialize()
 cfe_instance.run_unit_test(
     plot=True,
     print_fluxes=False,
-    plot_lims=list(range(1, 52600)),
+    plot_lims=list(range(1, 1310)),
     warm_up=True,
-    warmup_offset=12000,
+    warmup_offset=350,
     warmup_iteration=10,
 )
 cfe_instance.finalize(print_mass_balance=True)
