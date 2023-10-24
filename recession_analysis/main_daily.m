6da72217c82cd34a59b7beb852fab61fa3d899bf
RY4GIT <400mhs2@gmail.com>
2023-10-24 16:52:37 -0700

    rename:

--- recession_analysis/main_daily.m
+++ recession_analysis/main_daily.m
@@ -1,11 +1,11 @@
 addpath(genpath("C:\Users\flipl\dev\TOSSH\TOSSH_code"))
 
-data = readtable("C:\Program Files (x86)\HBV-light\Data\LittleWashita_daily\data\ptq.txt");
+data = readtable("C:\Program Files (x86)\HBV-light\Data\Coweeta\Data\ptq.txt");
 
-Q = data.Q_mm_hr_; %mm/h
+Q = data.Q_mm_d_; %mm/h
 Q(Q<0) = 0;
 t = datetime(num2str(data.Date), 'InputFormat', 'yyyyMMdd');
-P = data.P_mm_hr_; 
+P = data.P_mm_d_; 
 
 figure('pos',[100 100 350 200])
 plot(t,Q,'k-','linewidth',1.0)
@@ -18,10 +18,11 @@ xlabel('Date')
 ylabel('Rainfall [mm/hr]')
 
 [Recession_Parameters, recession_month] =sig_RecessionAnalysis(Q, t, 'fitting_type','linear', 'plot_results', true, 'fit_individual', false); 
-[Recession_Parameters, recession_month] =sig_RecessionAnalysis(Q, t, 'fitting_type','nonlinear', 'plot_results', true, 'fit_individual', true, 'recession_length', 3, 'filter_par', 2); 
+[Recession_Parameters, recession_month] =sig_RecessionAnalysis(Q, t, 'fitting_type','nonlinear', 'plot_results', true, 'fit_individual', true); %, 'recession_length', 3, 'filter_par', 2); 
 
+[MRC_num_segments,Segment_slopes]  = sig_MRC_SlopeChanges(Q, t,  'recession_length', 6, 'plot_results', true, 'seg_test', 2);
 
-[MRC_num_segments,Segment_slopes]  = sig_MRC_SlopeChanges(Q, t, 'recession_length', 6, 'filter_par',2, 'plot_results', true, 'seg_test', 2);
+%[MRC_num_segments,Segment_slopes]  = sig_MRC_SlopeChanges(Q, t, 'recession_length', 6, 'filter_par',2, 'plot_results', true, 'seg_test', 2);
 
 [Spearmans_rho]= sig_RecessionUniqueness(Q, t, 'plot_results', true) 
 [Spearmans_rho]= sig_RecessionUniqueness(Q, t,'plot_results', true, 'recession_length', 3, 'filter_par', 2); 
